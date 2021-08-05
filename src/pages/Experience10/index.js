0e7daade78ec06d3b435d1f2c266e007a6015f2f
Latsuj <justal.kevin@gmail.com>
2021-08-05 19:34:58 +0800

    [FEATURE] Hot Multiple Observer using ReplaySubject

---
+++ src/pages/Experience10/index.js
@@ -0,0 +1,65 @@
+/**
+ * @module Experiences/Experience0
+ */
+
+import React, { Profiler, useRef } from 'react'
+import { ReplaySubject, interval } from 'rxjs'
+
+const onRender = (id, phase, actualDuration) => {
+  console.log(id, phase, actualDuration)
+}
+
+const observable = interval(2000)
+const subject = new ReplaySubject(3)
+
+const observerA = {
+  next: (v) => console.log(`observerA: ${v}`)
+}
+const observerB = {
+  next: (v) => console.log(`observerB: ${v}`)
+}
+
+/**
+ * @function Experience
+ * @return {Object} Return the dom of the Experience
+ */
+const Experience = () => {
+  const subscriptionA = useRef()
+  const subscriptionB = useRef()
+
+  const handleClick = () => {
+    observable.subscribe(subject)
+  }
+
+  const handleSubscribeA = () => {
+    subscriptionA.current = subject.subscribe(observerA)
+  }
+
+  const handleSubscribeB = () => {
+    subscriptionB.current = subject.subscribe(observerB)
+  }
+
+  const handleUnsubscribe = () => {
+    subscriptionA.current.unsubscribe()
+    subscriptionB.current.unsubscribe()
+  }
+
+  return (
+    <Profiler id="Experience" onRender={onRender}>
+      <button onClick={handleClick}>
+        Wait 5s, call the Observable and then look at the console
+      </button>
+      <div>
+        Subscribe an observer to the subject and will return the last 3 values
+        of the observable if available
+      </div>
+      <button onClick={handleSubscribeA}>SubscribeA</button>
+      <button onClick={handleSubscribeB}>SubscribeB</button>
+      <button onClick={handleUnsubscribe}>
+        SubscribeB and directly return the current value of the subject
+      </button>
+    </Profiler>
+  )
+}
+
+export default Experience
